4827f8a0e42c088d258415463ac34d7dd3372565
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-26 04:38:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
-end
+  if array.length < 2
+    return array.join
+  elsif array.length == 2 
+    return  array.join (" and ")
+  elsif array.length == 3
+     return array.join (" , ") + "and "
+   else array.length > 3 
+     return 
+  end 
+  end 
+  
